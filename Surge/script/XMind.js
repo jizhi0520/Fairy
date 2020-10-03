4f7265dc330cd636b1a709325802bf92a012e5dd
0520 <72007390+jizhi0520@users.noreply.github.com>
2020-10-03 12:16:09 +0800

    Update XMind.js

--- Surge/script/XMind.js
+++ Surge/script/XMind.js
@@ -1,7 +1,8 @@
 var obj = JSON.parse($response.body);
 
 obj = {
-  "raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=",
+  "raw_data": "S0MY6Wu5wpkW52RE5XmMkSMfTBvnytTwIJODrtVDjnA0axrORbnv9gh1RC4W3/ejTfQhNBb7CVxxpbYnBBk2tHc4gAODhsuGpHkltYNL/P5dfORSpdbiNkAZr5aBBbHS/dNlaYjLYyBkq9Ohfe0QS9PeXOWLbDdNA6kqidLJysw=
+",
   "license": {
     "status": "sub",
     "expireTime": 3070928235000
