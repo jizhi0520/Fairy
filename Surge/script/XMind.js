864441decaf22cedc6ed3a9bf058396bd7f0041b
0520 <72007390+jizhi0520@users.noreply.github.com>
2020-10-03 12:13:03 +0800

    Update XMind.js

--- Surge/script/XMind.js
+++ Surge/script/XMind.js
@@ -2,7 +2,7 @@ var obj = JSON.parse($response.body);
 
 obj = {
   "raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=",
-  "license": {https://github.com/jizhi0520/Fairy
+  "license": {
     "status": "sub",
     "expireTime": 3070928235000
   },
